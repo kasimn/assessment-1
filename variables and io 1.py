0d98bcb9b76ce0a5e4edc83b2352dafe786f343e
kasimn <kasimraja305@gmail.com>
2024-11-08 14:20:52 +0000

    Add files via upload
    assessment 2

---
+++ variables and io 1.py
@@ -0,0 +1,8 @@
+first_num = int(input("Give me a number"))
+second_num = int(input("give me a number"))
+
+print(first_num + second_num)
+print(first_num - second_num)
+print(first_num * second_num)
+print(first_num // second_num)
+
